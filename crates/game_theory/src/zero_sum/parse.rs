69504983a39c321ce6d06555e6889332f16630c5
Petr Portnov <me@progrm-jarvis.ru>
2024-03-25 22:51:07 +0300

    feat(lab-1): allow spaces in matrix definitions

--- crates/game_theory/src/zero_sum/parse.rs
+++ crates/game_theory/src/zero_sum/parse.rs
@@ -5,9 +5,48 @@ use peg::{error::ParseError, str::LineCol};
 
 use super::{DGame, Game};
 
+impl FromStr for DGame<f64> {
+    type Err = FromStrError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(game::dgame(s)?)
+    }
+}
+
 #[derive(thiserror::Error, Debug)]
 #[error(transparent)]
 pub struct FromStrError(#[from] ParseError<LineCol>);
+
+peg::parser! {
+    grammar game() for str {
+        pub rule dgame<T: FromStr>() -> DGame<T>
+            = "{" rows:((_ v:row() _ { v }) ** ";") _ ";"? _ "}"
+        {?
+            Ok(Game(dmatrix_from_rows(rows)?))
+        }
+
+        rule row<T: FromStr>() -> Vec<T>
+            = "[" values:((_ v:float() _ { v }) ** ",") _ ","? _ "]"
+        {
+            values
+        }
+
+        rule _() = [' ' | '\t' | '\r' | '\n']*
+
+        rule float<T: FromStr>() -> T = num:$(sign()? finite_number()) {?
+            T::from_str(num).or(Err("failed to parse float number"))
+        }
+
+        rule sign() -> bool = "+" { true } / "-" { false }
+
+        rule digit() = ['0'..='9']
+
+        rule exp() = "e" sign()? digit()+
+
+        rule finite_number()
+            = (digit()+) / (digit()+ "." digit()*) / (digit()* "." digit()+) exp()?
+    }
+}
 
 /// Converts the rows into a dynamic matrix.
 fn dmatrix_from_rows<T>(rows: Vec<Vec<T>>) -> Result<DMatrix<T>, &'static str> {
@@ -31,41 +70,11 @@ fn dmatrix_from_rows<T>(rows: Vec<Vec<T>>) -> Result<DMatrix<T>, &'static str> {
         }
     }
 
-    Ok(DMatrix::from_vec_storage(VecStorage::new(Dyn(row_count), Dyn(row_len), data)))
-}
-
-peg::parser! {
-    grammar game() for str {
-        pub rule dynamic() -> DGame<f64> = "{" _ rows:(row() ** ";") _ "}" {?
-            Ok(Game(dmatrix_from_rows(rows)?))
-        }
-
-        // TODO: allow spaces between values
-        rule row() -> Vec<f64> = "[" _ values:(float() ** ",") _ "]" { values }
-
-        rule _() = [' ' | '\t' | '\r' | '\n']*
-
-        rule float() -> f64 = num:$(sign()? finite_number()) {?
-            f64::from_str(num).or(Err("failed to parse float number"))
-        }
-
-        rule sign() -> bool = "+" { true } / "-" { false }
-
-        rule digit() = ['0'..='9']
-
-        rule exp() = "e" sign()? digit()+
-
-        rule finite_number()
-            = (digit()+) / (digit()+ "." digit()*) / (digit()* "." digit()+) exp()?
-    }
-}
-
-impl FromStr for DGame<f64> {
-    type Err = FromStrError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(game::dynamic(s)?)
-    }
+    Ok(DMatrix::from_vec_storage(VecStorage::new(
+        Dyn(row_count),
+        Dyn(row_len),
+        data,
+    )))
 }
 
 #[cfg(test)]
@@ -77,17 +86,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn print_deserialization() {
-        let g = game::dynamic("{[1,2,3];[4,5,6]}").unwrap();
-        println!("{}", g);
-        for row in g.0.row_iter() {
-            println!("Row: {row}");
-        }
+    fn multi_line_f64_matrix() {
         assert_eq!(
-            game::dynamic("{[1,2,3];[4,5,6]}"),
+            game::dgame(
+                "{
+                    [1, 2, 3];
+                    [4, 5, 6];
+                }"
+            ),
             Ok(Game(dmatrix![
                     1., 2., 3.;
                     4., 5., 6.;
+            ])),
+        );
+    }
+
+    #[test]
+    fn single_line_f32_matrix() {
+        assert_eq!(
+            game::dgame("{[10,20] ; [30,40] ; [50,60] ; [70,80]}"),
+            Ok(Game(dmatrix![
+                    10f32, 20f32;
+                    30f32, 40f32;
+                    50f32, 60f32;
+                    70f32, 80f32;
             ])),
         );
     }
