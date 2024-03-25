e1f8cce066119f4a8c70916fb3a0278036799dcd
Petr Portnov <me@progrm-jarvis.ru>
2024-03-25 18:16:28 +0300

    chore(nix): configure Flakes

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Game Theory labs";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+          config.allowUnfree = true;
+        };
+        rust = (pkgs.rustChannelOf { channel = "stable"; }).default.override {
+          extensions = [ "rust-analyzer" "rust-src" "clippy" ];
+        };
+      in rec {
+        devShell = pkgs.mkShell { nativeBuildInputs = with pkgs; [ rust ]; };
+      });
+}
