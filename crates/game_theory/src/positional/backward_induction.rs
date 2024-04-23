050a52fac2503474baeef88061ce31135f249450
Petr Portnov <me@progrm-jarvis.ru>
2024-04-23 14:51:54 +0300

    feat(lab-4): support wins in printing

--- crates/game_theory/src/positional/backward_induction.rs
+++ crates/game_theory/src/positional/backward_induction.rs
@@ -1,6 +1,6 @@
 use std::{
     fmt,
-    fmt::Formatter,
+    fmt::{Debug, Display, Formatter},
     io::{self, Write},
     num::{NonZeroU8, Wrapping},
 };
@@ -21,7 +21,7 @@ struct Loc {
 #[derive(Debug)]
 struct Node<T> {
     loc: Loc,
-    win: Option<Vec<T>>,
+    win: Option<Prize<T>>,
 }
 
 #[derive(Debug)]
@@ -36,7 +36,10 @@ pub struct BackwardInductionGame<T> {
 }
 
 impl<T> BackwardInductionGame<T> {
-    pub fn reduce(&mut self, mut out: impl Write) {
+    pub fn reduce(&mut self, mut out: impl Write)
+    where
+        T: Ord + Copy + Display,
+    {
         self.print_current(&mut out).unwrap();
         // for layer in (0..self.layers.len() - 1).rev() {
         //     let current_layer = &self.layers[layer];
@@ -111,18 +114,21 @@ impl<T> BackwardInductionGame<T> {
         }
 
         for node in &mut layers.last_mut().unwrap().nodes {
-            node.win = Some(
+            node.win = Some(Prize(
                 players
                     .iter()
                     .map(|_| generator.gen_range(range.clone()))
                     .collect(),
-            );
+            ));
         }
 
         Some(Self { layers })
     }
 
-    pub fn print_current(&self, out: &mut impl Write) -> io::Result<()> {
+    pub fn print_current(&self, out: &mut impl Write) -> io::Result<()>
+    where
+        T: Ord + Copy + Display,
+    {
         writeln!(out, "```mermaid")?;
         writeln!(out, "flowchart LR")?;
 
@@ -131,20 +137,64 @@ impl<T> BackwardInductionGame<T> {
             let prev_layer = &self.layers[layer - 1];
             let cur_layer = &self.layers[layer];
 
-            let mut prev_index = Wrapping(usize::MAX);
+            let mut prev_index = 0;
+
+            struct Win<T> {
+                player: Player,
+                from_uid: usize,
+                to_uid: usize,
+                prize: Prize<T>,
+            }
+            impl<T: Ord + Copy + Display> Win<T> {
+                fn commit(wins: &Vec<Self>, out: &mut impl Write) -> io::Result<()> {
+                    let Some(max_win) = wins
+                        .iter()
+                        .map(|Win { player, prize, .. }| prize.0[player.0])
+                        .max()
+                    else {
+                        return Ok(());
+                    };
+
+                    for Win {
+                        from_uid,
+                        to_uid,
+                        prize,
+                        ..
+                    } in wins
+                        .iter()
+                        .filter(|Win { player, prize, .. }| prize.0[player.0] == max_win)
+                    {
+                        writeln!(out, "    {} ===>|{}| {}", to_uid, prize, from_uid)?;
+                    }
+
+                    Ok(())
+                }
+            }
+            let mut wins = vec![];
             for cur_index in 0..cur_layer.nodes.len() {
                 let cur = &cur_layer.nodes[cur_index];
                 if cur.loc.strat == 1 {
                     prev_index += 1;
+                    Win::commit(&wins, out)?;
+                    wins.clear();
                 }
 
-                let prev = &prev_layer.nodes[prev_index.0];
+                let prev = &prev_layer.nodes[prev_index - 1];
                 writeln!(
                     out,
                     "    {0} ---> {1}(({2}))",
                     prev.loc.uid, cur.loc.uid, cur.loc.player
                 )?;
+                if let Some(prize) = cur.win.clone() {
+                    wins.push(Win {
+                        from_uid: prev.loc.uid,
+                        to_uid: cur.loc.uid,
+                        player: prev.loc.player,
+                        prize,
+                    });
+                }
             }
+            Win::commit(&wins, out)?;
         }
         writeln!(out, "```")?;
 
@@ -155,11 +205,34 @@ impl<T> BackwardInductionGame<T> {
 #[derive(Clone, Copy, Debug)]
 struct Player(usize);
 
-impl fmt::Display for Player {
+impl Display for Player {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         use fmt::Write;
 
         let Self(player) = self;
         f.write_char((b'A' + *player as u8) as char)
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Prize<T>(Vec<T>);
+
+impl<T: Display> Display for Prize<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        use fmt::Write;
+
+        let Self(wins) = self;
+        let mut not_first = false;
+        for win in wins {
+            if not_first {
+                f.write_str(", ")?;
+            } else {
+                not_first = true;
+            }
+
+            write!(f, "{win}")?;
+        }
+
+        Ok(())
     }
 }
