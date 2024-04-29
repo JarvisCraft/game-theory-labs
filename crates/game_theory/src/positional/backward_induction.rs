96e6c14badc2739291dbcb730bc0855227ac7ed4
Petr Portnov <me@progrm-jarvis.ru>
2024-04-30 02:31:32 +0300

    feat(lab-4): add finalizing coloring

--- crates/game_theory/src/positional/backward_induction.rs
+++ crates/game_theory/src/positional/backward_induction.rs
@@ -3,7 +3,7 @@ use std::{
     fmt,
     fmt::{Debug, Display, Formatter},
     io::{self, Write},
-    num::NonZeroU8,
+    num::{NonZeroU8, Wrapping},
 };
 
 use rand::{
@@ -22,13 +22,12 @@ struct Loc {
 #[derive(Debug)]
 struct Node<T> {
     loc: Loc,
-    win: Option<Prize<T>>,
+    prize: Option<Prize<T>>,
 }
 
 #[derive(Debug)]
 struct Layer<T> {
     nodes: Vec<Node<T>>,
-    size: usize,
 }
 
 #[derive(Debug)]
@@ -39,11 +38,11 @@ pub struct BackwardInductionGame<T> {
 impl<T> BackwardInductionGame<T> {
     pub fn reduce(&mut self, mut out: impl Write) -> io::Result<()>
     where
-        T: Ord + Copy + Display,
+        T: Ord + Copy + Debug + Display,
     {
         writeln!(out, "# Iteration #0")?;
         writeln!(out)?;
-        self.print_current(&mut out).unwrap();
+        self.print_current(&mut out)?;
 
         let mut iteration = 0;
         for layer in (1..self.layers.len()).rev() {
@@ -55,12 +54,12 @@ impl<T> BackwardInductionGame<T> {
             for node in &self.layers[layer].nodes {
                 wins.entry(node.loc.parent)
                     .or_default()
-                    .push(node.win.clone().unwrap());
+                    .push(node.prize.clone().unwrap());
             }
             for (parent_idx, prizes) in wins {
                 let parent = &mut self.layers[layer - 1].nodes[parent_idx];
                 let parent_player = parent.loc.player.0;
-                parent.win = Some(
+                parent.prize = Some(
                     prizes
                         .into_iter()
                         .max_by_key(|prize| prize.0[parent_player])
@@ -100,9 +99,8 @@ impl<T> BackwardInductionGame<T> {
                     strat: 0,
                     parent: 0,
                 },
-                win: None,
+                prize: None,
             }],
-            size: layer_size,
         });
 
         for layer in 0..depth {
@@ -129,17 +127,14 @@ impl<T> BackwardInductionGame<T> {
                         strat: strat + 1,
                         parent: parent_index - 1,
                     },
-                    win: None,
+                    prize: None,
                 });
             }
-            layers.push(Layer {
-                nodes,
-                size: layer_size,
-            });
+            layers.push(Layer { nodes });
         }
 
         for node in &mut layers.last_mut().unwrap().nodes {
-            node.win = Some(Prize(
+            node.prize = Some(Prize(
                 players
                     .iter()
                     .map(|_| generator.gen_range(range.clone()))
@@ -152,12 +147,14 @@ impl<T> BackwardInductionGame<T> {
 
     pub fn print_current(&self, out: &mut impl Write) -> io::Result<()>
     where
-        T: Ord + Copy + Display,
+        T: Ord + Copy + Debug + Display,
     {
         writeln!(out, "```mermaid")?;
         writeln!(out, "flowchart LR")?;
 
-        writeln!(out, "    0(({}))", Player(0))?;
+        writeln!(out, "    0(({}0))", Player(0))?;
+
+        let mut link_id = 0;
         let max_layer = self.layers.len() - 1;
         for layer in 1..self.layers.len() {
             let prev_layer = &self.layers[layer - 1];
@@ -170,13 +167,13 @@ impl<T> BackwardInductionGame<T> {
                 let cur = &cur_layer.nodes[cur_index];
                 if cur.loc.strat == 1 {
                     prev_index += 1;
-                    Win::commit(&wins, out)?;
+                    link_id += Win::commit(&wins, out)?;
                     wins.clear();
                 }
 
                 let prev = &prev_layer.nodes[prev_index - 1];
                 if layer == max_layer {
-                    if let Some(prize) = &cur.win {
+                    if let Some(prize) = &cur.prize {
                         writeln!(out, "    {} --> {}[[{}]]", prev.loc.uid, cur.loc.uid, prize)?;
                     } else {
                         writeln!(out, "    {} ---> {}[[_]]", prev.loc.uid, cur.loc.uid)?;
@@ -184,11 +181,13 @@ impl<T> BackwardInductionGame<T> {
                 } else {
                     writeln!(
                         out,
-                        "    {0} ---> {1}(({2}))",
+                        "    {0} ---> {1}(({2}{1}))",
                         prev.loc.uid, cur.loc.uid, cur.loc.player
                     )?;
                 }
-                if let Some(prize) = cur.win.clone() {
+                link_id += 1;
+
+                if let Some(prize) = cur.prize.clone() {
                     wins.push(Win {
                         from_uid: prev.loc.uid,
                         to_uid: cur.loc.uid,
@@ -197,8 +196,41 @@ impl<T> BackwardInductionGame<T> {
                     });
                 }
             }
-            Win::commit(&wins, out)?;
+            link_id += Win::commit(&wins, out)?;
         }
+
+        if let Some(root_win) = self.layers[0].nodes[0].prize.as_ref() {
+            let mut parents = HashMap::from([(0usize, Wrapping(0xFF0000u32))]);
+
+            for layer_id in 1..self.layers.len() {
+                let layer = &self.layers[layer_id];
+                for node in &layer.nodes {
+                    if let Some(prize) = &node.prize {
+                        let uid = node.loc.uid;
+                        let parent_uid = &self.layers[layer_id - 1].nodes[node.loc.parent].loc.uid;
+                        if prize == root_win {
+                            let parent_color = parents
+                                .get_mut(parent_uid)
+                                .unwrap_or_else(|| panic!("Parent {parent_uid} for {uid}"));
+
+                            writeln!(out, "    {parent_uid} ===> {uid}")?;
+                            writeln!(
+                                out,
+                                "    linkStyle {link_id} stroke:#{0:06x},color:#{0:06x},stroke-width:4px",
+                                parent_color.0 & 0xFFFFFF,
+                            )?;
+                            let color = *parent_color;
+                            link_id += 1;
+
+                            // A big prime number to mix colors.
+                            *parent_color *= 82_589_933;
+                            parents.insert(uid, color);
+                        }
+                    }
+                }
+            }
+        }
+
         writeln!(out, "```")?;
 
         Ok(())
@@ -217,13 +249,11 @@ impl Display for Player {
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 struct Prize<T>(Vec<T>);
 
 impl<T: Display> Display for Prize<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        use fmt::Write;
-
         let Self(wins) = self;
         let mut not_first = false;
         for win in wins {
@@ -247,15 +277,16 @@ struct Win<T> {
     prize: Prize<T>,
 }
 impl<T: Ord + Copy + Display> Win<T> {
-    fn commit(wins: &Vec<Self>, out: &mut impl Write) -> io::Result<()> {
+    fn commit(wins: &[Self], out: &mut impl Write) -> io::Result<usize> {
         let Some(max_win) = wins
             .iter()
             .map(|Win { player, prize, .. }| prize.0[player.0])
             .max()
         else {
-            return Ok(());
+            return Ok(0);
         };
 
+        let mut new_links = 0;
         for Win {
             from_uid,
             to_uid,
@@ -265,9 +296,10 @@ impl<T: Ord + Copy + Display> Win<T> {
             .iter()
             .filter(|Win { player, prize, .. }| prize.0[player.0] == max_win)
         {
-            writeln!(out, "    {} ===>|\"{}\"| {}", to_uid, prize, from_uid)?;
+            writeln!(out, "    {} -.->|\"{}\"| {}", to_uid, prize, from_uid)?;
+            new_links += 1;
         }
 
-        Ok(())
+        Ok(new_links)
     }
 }
