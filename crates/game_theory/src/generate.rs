75659871791d479f19b62d87096c02fcd0cf03be
Petr Portnov <me@progrm-jarvis.ru>
2024-05-06 03:02:46 +0300

    feat(lab-6): implement the task

--- crates/game_theory/src/generate.rs
+++ crates/game_theory/src/generate.rs
@@ -1,4 +1,4 @@
-use nalgebra::{DMatrix, Dyn, VecStorage};
+use nalgebra::{DMatrix, DVector, Dyn, VecStorage, U1};
 use rand::{
     distributions::uniform::{SampleRange, SampleUniform},
     Rng,
@@ -14,6 +14,20 @@ pub fn random_matrix<T: SampleUniform>(
         Dyn(rows),
         Dyn(columns),
         (0..rows * columns)
+            .map(|_| random.gen_range(range.clone()))
+            .collect(),
+    ))
+}
+
+pub fn random_vector<T: SampleUniform>(
+    mut random: impl Rng,
+    length: usize,
+    range: impl SampleRange<T> + Clone,
+) -> DVector<T> {
+    DVector::from_vec_storage(VecStorage::new(
+        Dyn(length),
+        U1,
+        (0..length)
             .map(|_| random.gen_range(range.clone()))
             .collect(),
     ))
---
+++ labs/lab-06-informational_warfare/src/main.rs
@@ -0,0 +1,134 @@
+use std::ops::DivAssign;
+
+use clap::Parser;
+use game_theory::generate::{random_matrix, random_vector};
+use nalgebra::{DMatrix, DVector};
+use rand::prelude::*;
+use rand_chacha::ChaCha20Rng;
+use tracing::{debug, error, info};
+
+const DIMENSION: usize = 10;
+
+fn main() {
+    let Options {
+        x_min,
+        x_max,
+        agent_min,
+        agent_max,
+        player_1_agents,
+        player_2_agents,
+        epsilon,
+        seed,
+    } = Options::parse();
+
+    tracing_subscriber::fmt::init();
+
+    if x_min >= x_max {
+        error!("Minimal X value = {x_min} should be smaller than maximal X value = {x_max}");
+        return;
+    }
+
+    if agent_min >= agent_max {
+        error!(
+            "Minimal agent value = {agent_min} should be smaller than maximal agent value = {agent_max}"
+        );
+        return;
+    }
+
+    if player_1_agents + player_2_agents > DIMENSION {
+        error!("The sum of player 1 agents = {player_1_agents} and player 2 agents = {player_2_agents} should not exceed {DIMENSION}");
+        return;
+    }
+
+    let mut random = if let Some(seed) = seed {
+        ChaCha20Rng::seed_from_u64(seed)
+    } else {
+        ChaCha20Rng::from_entropy()
+    };
+
+    let mut a = random_matrix(&mut random, DIMENSION, DIMENSION, 0. ..=1.);
+    for mut row in a.row_iter_mut() {
+        row.div_assign(row.sum());
+    }
+    info!("A = {a:.03}");
+
+    let x = random_vector(&mut random, DIMENSION, x_min..=x_max);
+    info!("x(0) = {:.03}", x.transpose());
+    let (iteration, x) = simulate(&a, x, epsilon);
+    info!("x({iteration}) = {:.03}", x.transpose());
+
+    let mut agents: Vec<_> = (0..DIMENSION).collect();
+    let agents_of_1: Vec<_> = agents
+        .choose_multiple(&mut random, player_1_agents)
+        .copied()
+        .collect();
+
+    agents.retain(|agent| !agents_of_1.contains(agent));
+    let agents_of_2: Vec<_> = agents
+        .choose_multiple(&mut random, player_2_agents)
+        .copied()
+        .collect();
+
+    println!(
+        "Agents of Player 1: {:?}",
+        agents_of_1.iter().map(|i| i + 1).collect::<Vec<_>>()
+    );
+    println!(
+        "Agents of Player 2: {:?}",
+        agents_of_2.iter().map(|i| i + 1).collect::<Vec<_>>()
+    );
+
+    let u = random.gen_range(agent_min..=agent_max);
+    let v = -random.gen_range(agent_min..=agent_max);
+    let mut x = random_vector(&mut random, DIMENSION, x_min..=x_max);
+    for idx in agents_of_1 {
+        x[idx] = u;
+    }
+    for idx in agents_of_2 {
+        x[idx] = v;
+    }
+
+    info!("x(0) = {:.03}", x.transpose());
+    let (iteration, x) = simulate(&a, x, epsilon);
+    info!("x({iteration}) = {:.03}", x.transpose());
+}
+
+fn simulate(a: &DMatrix<f64>, mut x: DVector<f64>, epsilon: f64) -> (usize, DVector<f64>) {
+    let mut iteration = 0;
+    while x.max() - x.min() > epsilon {
+        iteration += 1;
+        x = a * &x;
+        debug!("x({iteration}) = {}", x.transpose());
+    }
+    (iteration, x)
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[clap(allow_negative_numbers = true)]
+struct Options {
+    #[arg(long, default_value_t = 1.)]
+    x_min: f64,
+
+    #[arg(long, default_value_t = 20.)]
+    x_max: f64,
+
+    #[arg(long, default_value_t = 1.)]
+    agent_min: f64,
+
+    #[arg(long, default_value_t = 20.)]
+    agent_max: f64,
+
+    #[arg(long, default_value_t = 2)]
+    player_1_agents: usize,
+
+    #[arg(long, default_value_t = 2)]
+    player_2_agents: usize,
+
+    #[arg(long, short, default_value_t = 1E-6)]
+    epsilon: f64,
+
+    /// Random generator seed
+    #[arg(long)]
+    seed: Option<u64>,
+}
